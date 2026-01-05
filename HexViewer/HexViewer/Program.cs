a48bb0c77596350ebabc9829d5e8c482eb52d7ec
Gabriel Trip <tripgabriel30@gmail.com>
2026-01-05 16:44:00 +0200

    hex viewer

---
+++ HexViewer/HexViewer/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+
+class HexViewer
+{
+    static void Main()
+    {
+        string filePath = @"path";
+        int octetsPerLine = 16;
+
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine("error - file not found");
+            return;
+        }
+
+        using (FileStream fluxFile = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+        {
+            byte[] buffer = new byte[octetsPerLine];
+            long currentPosition = 0;
+            int nrOctetsRead;
+
+            while ((nrOctetsRead = fluxFile.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                Console.Write($"{currentPosition:X8}: ");
+
+                for (int i = 0; i < octetsPerLine; i++)
+                {
+                    if (i < nrOctetsRead)
+                    {
+                        Console.Write($"{buffer[i]:X2} ");
+                    }
+                    else
+                    {
+                        Console.Write("   ");
+                    }
+                }
+
+                Console.Write("| ");
+
+                for (int i = 0; i < nrOctetsRead; i++)
+                {
+                    char character = (char)buffer[i];
+
+                    if (character >= 32 && character <= 126)
+                    {
+                        Console.Write(character);
+                    }
+                    else
+                    {
+                        Console.Write(".");
+                    }
+                }
+
+                Console.WriteLine();
+                currentPosition += nrOctetsRead;
+            }
+        }
+    }
+}
---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,14 @@
+﻿//Setul 3 (sortari)
+
+static void P1()
+{
+    int n;
+    n = int.Parse(Console.ReadLine());
+    int[] arr = new int[n];
+    string input = Console.ReadLine();
+    foreach( string number in input.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+    {
+        int a = int.Parse(number);
+        var[i]
+    }
+}
